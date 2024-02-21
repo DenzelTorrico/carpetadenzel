469c8b23315d929dca049f0b9f2084e7cbed9d3e
DenzelTorrico <denzel_silva@hotmail.com>
2024-02-21 18:12:13 -0500

    new diseño

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
 var perros = []
+var images = []
 fetch("https://dog.ceo/api/breeds/list/all").then(response=>response.json())
 .then(data=>{
    perros = Object.keys(data.message)
    let dogs = document.getElementById("dogs")
-
-   perros.forEach(element => {
-    let tr = document.createElement("tr")
-    let td = document.createElement("td")
-    tr.appendChild(td)
-    td.textContent = element
-    dogs.appendChild(tr)
-   });
+    fetch("https://dog.ceo/api/breed/hound/images").then(responseimages=>responseimages.json())
+    .then(dataimages=>{
+        var images = dataimages.message
+        for (let i=0; i<10; i++){
+            let tr = document.createElement("tr")
+            let td = document.createElement("td")
+            let tdimg = document.createElement("td")
+            let img = document.createElement("img")
+            img.className = "img-fluid rounded"
+            img.src = images[i]
+            img.style.height = "200px"
+            tdimg.appendChild(img)
+            tr.appendChild(td)
+            tr.appendChild(tdimg)
+            td.textContent = perros[i]
+            dogs.appendChild(tr)
+        }
+    }).catch(error=>{
+        console.log(error)
+    });
+   
 
 })
 
